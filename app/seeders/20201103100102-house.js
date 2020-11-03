1b82b46caedc507b614138d7d1f542a4203c0496
BuildTools <unconfigured@null.spigotmc.org>
2020-11-03 13:02:38 +0100

    Edited house seeder

--- app/seeders/20201103100102-house.js
+++ app/seeders/20201103100102-house.js
@@ -1,14 +1,19 @@
 'use strict';
 
+const Tools = require('../utils/Tools');
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const user_id = await queryInterface.rawSelect('Users', {}, ['id']);
     await queryInterface.bulkInsert('houses', [{
+      id: Tools.uuid(),
       name: 'Maison 1',
       user_id: user_id[0],
       created_at: Sequelize.fn('NOW'),
       updated_at: Sequelize.fn('NOW'),
-    }, {
+    }], {});
+    await queryInterface.bulkInsert('houses', [{
+      id: Tools.uuid(),
       name: 'Maison 2',
       user_id: user_id[0],
       created_at: Sequelize.fn('NOW'),
