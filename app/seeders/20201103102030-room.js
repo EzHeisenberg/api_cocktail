a260a9f4cabafb528b465f3009ac0679eb307109
BuildTools <unconfigured@null.spigotmc.org>
2020-11-03 11:46:00 +0100

    Fixed wrong typo in room table name in its migration

--- app/seeders/20201103102030-room.js
+++ app/seeders/20201103102030-room.js
@@ -2,19 +2,18 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const users = await queryInterface.rawSelect('room', {}, ['id']);
+    const users = await queryInterface.rawSelect('rooms', {}, ['id']);
     for (let user in users) {
-
+      await queryInterface.bulkInsert('houses', [{
+        name: 'Maison 1',
+        user_id: user,
+        created_at: Sequelize.fn('NOW'),
+        updated_at: Sequelize.fn('NOW'),
+      }], {});
     }
-    await queryInterface.bulkInsert('houses', [{
-      name: 'Maison 1',
-      user_id: user_id[0],
-      created_at: Sequelize.fn('NOW'),
-      updated_at: Sequelize.fn('NOW'),
-    }], {});
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('room', null, {});
+    await queryInterface.bulkDelete('rooms', null, {});
   }
 };
