847b6293b9a704948e95df116ead30a617574a79
BuildTools <unconfigured@null.spigotmc.org>
2020-11-03 11:49:04 +0100

    Fixed error in room seeder

--- app/seeders/20201103102030-room.js
+++ app/seeders/20201103102030-room.js
@@ -2,11 +2,12 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const users = await queryInterface.rawSelect('rooms', {}, ['id']);
+    const users = await queryInterface.rawSelect('Users', {}, ['id']);
+    console.log(users);
     for (let user in users) {
-      await queryInterface.bulkInsert('houses', [{
-        name: 'Maison 1',
-        user_id: user,
+      await queryInterface.bulkInsert('rooms', [{
+        name: 'Salon',
+        house_id: user,
         created_at: Sequelize.fn('NOW'),
         updated_at: Sequelize.fn('NOW'),
       }], {});
