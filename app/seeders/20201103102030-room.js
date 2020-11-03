5b29e8daf7a5cfc5f89afc59fbe6a0bbf1d7a317
BuildTools <unconfigured@null.spigotmc.org>
2020-11-03 11:54:00 +0100

    Fixed error in room seed and added random room names

--- app/seeders/20201103102030-room.js
+++ app/seeders/20201103102030-room.js
@@ -2,11 +2,13 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const houses = await queryInterface.rawSelect('houses', {}, ['id']);
+    const rooms = ['Salon', 'Cuisine', 'Salle à manger', 'Salle de bain'];
+    const houses = await queryInterface.rawSelect('houses', {}, []);
+    console.log(houses);
     for (let house in houses) {
       await queryInterface.bulkInsert('rooms', [{
-        name: 'Salon',
-        house_id: houses,
+        name: rooms[Math.floor(Math.random() * Math.floor(3))],
+        house_id: house.id,
         created_at: Sequelize.fn('NOW'),
         updated_at: Sequelize.fn('NOW'),
       }], {});
